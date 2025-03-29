a31a56769d2f83754484959d8185567555bf761a
Rajadurai-dgl <rajnives@gmail.com>
2025-03-29 17:39:47 +0530

    Add files via upload
    Files has been added.

---
+++ Chrome.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args)
+	{
+		float version=91.0f;
+		System.out.println(version);
+		
+		String name="Google";
+		System.out.println(name);
+		
+		boolean beta =false;
+		System.out.println(beta);
+		
+		int releaseYear=2008;
+		System.out.println(releaseYear);
+		
+		char shortcutKey='C';
+		System.out.println(shortcutKey);
+
+	}
+
+}
